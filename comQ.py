d8b80d7ca84dcb98b60ce08511a6dbea813a20ce
david <cdavidlopez@hotmail.com>
2019-08-08 14:44:51 -0500

    fin

--- comQ.py
+++ comQ.py
@@ -15,21 +15,32 @@ def resta(x,y):
     #3
     c=(x[0]-y[0],x[1]-y[1])
     return c
-def division():
+def division(x,y):
     #4
-    None
+    a1=producto(x,(y[0],-y[1]))
+    a2=producto(y,(y[0],-y[1]))
+    c= (a1[0]/a2[0],a1[1]/a2[0])
+    return c
 def modulo(real,imaginario):
     #5
     c= (real**2 + imaginario**2)**(1/2)
     return c
-    
 def conjugado(real,imaginario):
     #6
     c=(real,-imaginario)
     return c
-def polar_cartesiano():
+def polar_cartesiano(n,teta):
     #7
-    None
+    x=n*math.cos(math.radians(teta))
+    y=n*math.sin(math.radians(teta))
+    c=(x,y)
+    return c
+def cartesiano_polar(real,imaginario):
+    #7.5
+    n=modulo(real,imaginario)
+    teta= math.degrees(math.atan2(imaginario,real))
+    c=(n,teta)
+    return c
 def fase(real,imaginario):
     #8
     c = math.atan2(imaginario,real)
--- prueba.py
+++ prueba.py
@@ -1,3 +1,53 @@
 import comQ;
+import unittest;
 
-print(comQ.resta((1,4),(5,1)))
+class TestComQ(unittest.TestCase):
+    def testSuma(self):
+        c1=(1,5)
+        c2=(2,-3)
+        r=comQ.suma(c1,c2)
+        res=(3,2)
+        self.assertEqual(r,res)
+    def testProducto(self):
+        c1=(1,4)
+        c2=(5,1)
+        r=comQ.producto(c1,c2)
+        res=(1,21)
+        self.assertEqual(r,res)
+    def testResta(self):
+        c1=(1,5)
+        c2=(2,-3)
+        r=comQ.resta(c1,c2)
+        res=(-1,8)
+        self.assertEqual(r,res)
+    def testDivision(self):
+        c1=(3,2)
+        c2=(-1,2)
+        r=comQ.division(c1,c2)
+        res=(1/5,-8/5)
+        self.assertEqual(r,res)
+    def testModulo(self):
+        c1=(1,-3)
+        r=comQ.modulo(c1[0],c1[1])
+        res=(c1[0]**2+c1[1]**2)**(1/2)
+        self.assertEqual(r,res)
+    def testConjugado(self):
+        c1=(4,2)
+        r=comQ.conjugado(c1[0],c1[1])
+        res=(4,-2)
+        self.assertEqual(r,res)
+    def testPolarCart(self):
+        r=comQ.polar_cartesiano(13.0, 22.619864948040426)
+        res=(12,5)
+        self.assertEqual(r,res)
+    def testCartPolar(self):
+        r=comQ.cartesiano_polar(12,5)
+        res=(13.0, 22.619864948040426)
+        self.assertEqual(r,res)
+    def testFase(self):
+        r=comQ.fase(1,-1)
+        res=-0.7853981633974483
+        self.assertEqual(r,res)
+    
+if __name__ == "__main__":
+    unittest.main()
