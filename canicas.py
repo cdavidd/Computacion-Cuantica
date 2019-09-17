1d51b4900af0720d5339bfd4e4d9adf583e36678
cdavidd <cdavidavidlopez@hotmail.com>
2019-09-17 14:08:43 -0500

    sistema

--- canicas.py
+++ canicas.py
@@ -10,3 +10,33 @@ def canicas(m,v,n):
             r.append(cont)
         v=r
     return v
+
+def tensor (m1,m2):
+    res= [[0 for x in range(len(m1[0])*len(m2[0]))] for y in range (len(m1)*len(m2))]
+    for i in range(len(m1)):
+        for j in range (len(m1[0])):
+            for x in range(len(m2)):
+                for y in range(len(m2[0])):
+                    res[i*len(m2)+x][j*len(m2[0])+y]= m1[i][j]*m2[x][y]
+    return res
+def vecc(v1,v2):
+    v=[]
+    for x in range(len(v1)):
+        for y in range(len(v2)):
+            v.append(v1[x]*v2[y])
+    return v
+
+def system(m1,m2,v1,v2,n):
+    m=tensor(m1,m2)
+    v=vecc(v1,v2)
+    for x in range(n):
+        r=[]
+        for i in range(len(m)):
+            cont=0
+            for j in range(len(m[0])):
+                cont+=m[i][j]*v[j]
+            r.append(cont)
+        v=r
+    for x in range(len(v)):
+        v[x]= round(v[x]*100,2)
+    return v
